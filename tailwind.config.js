35253f0198047f8c3607b35090768f7a2783d32d
Temur Coder <temurnarzullayev810@gmail.com>
2024-12-10 17:48:25 +0300

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
-/ @type {import('tailwindcss').Config} */
+/ @type {import('tailwindcss').Config} */;
 module.exports = {
   content: ["./public//*.{html,js}", "index.html"],
   theme: {
     extend: {
-      backgroundColor:{
-        "header-color": "#9EAFC2",
+      backgroundColor: {
+        "header-color": "#eceef3",
       },
-      textColor:{
+      textColor: {
         "text-grey": "#7f7f7f",
-      }
+      },
     },
   },
   plugins: [],
-}
+};
