5f7b64fc12d917b99a2b0541463b35880d6d74d1
Temur Coder <temurnarzullayev810@gmail.com>
2024-12-12 17:55:37 +0300

    add click

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let close = document.querySelector('.close');
+let menu_bar = document.querySelector('.menu-bar');
+let menu = document.querySelector('.menu');
+
+menu_bar.addEventListener('click', ()=>{
+    menu.classList.add('hidden');
+})
